9391be4f1959ad50a59cc036f37d9bbb0825e532
Alexander Rozman <biolog71@gmail.com>
2025-10-24 15:19:54 +0300

    cpu exceptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
 
 #![no_std]
 #![cfg_attr(test, no_main)]
+#![feature(abi_x86_interrupt)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 pub mod vga_buffer;
 pub mod serial;
+pub mod interrupts;
 
 use core::panic::PanicInfo;
+
+pub fn init() {
+    interrupts::init_idt();
+}
 
 pub fn test_runner(tests: &[&dyn Testable]){
     serial_println!("Running {} tests", tests.len());
@@ -66,6 +72,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,10 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    blog_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
